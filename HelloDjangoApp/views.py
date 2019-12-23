2b341fc0e266c186f4beaf095a548ee579db3d7d
anat mas <mastryukov1990@gmail.com>
2019-12-23 03:01:25 +0300

    nn

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Leonid")
+    now = datetime.now()
 
-# Create your views here.
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {   'title' : "Hi",
+            'message' : "hii",
+            'content':" on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+
+
