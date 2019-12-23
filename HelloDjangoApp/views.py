faa2055409031a3893d7178682e37cec780a3f5f
anat mas <mastryukov1990@gmail.com>
2019-12-23 13:46:55 +0300

    Color added

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -10,7 +10,7 @@ def index(request):
         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {   'title' : "Hi",
-            'message' : "hii",
+            'message' : "hii, Nikita",
             'content':" on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
